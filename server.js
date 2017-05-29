5118dd504a0d9c76e8a920ca0fcbdd642abf25fc
Pamela Argentino <argentinopam@hotmail.com>
2017-05-29 15:52:28 -0300

    Implement Redux on the server

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ import path from 'path';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import reducers from './src/reducers';
 import App from './src/App';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const server = new Express();
 server.use(Express.static(path.resolve(__dirname, 'build')));
@@ -19,11 +26,22 @@ server.get('/api/stores', function (req, res, next) {
   ]);
 });
 
-server.get('*', function(req, res, next) {
+server.get('*', async function(req, res, next) {
+  const response = await axios.get('http://localhost:3333/api/stores');
+
+  const store = createStore(reducers, {
+    stores: {
+      all: response.data,
+      loading: false
+    }
+  }, applyMiddleware(ReduxThunk));
+
   const html = renderToString(
-    <StaticRouter location={req.url} context={{}}>
-      <App />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        <App />
+      </StaticRouter>
+    </Provider>
   );
 
   res.render('index', { html });
